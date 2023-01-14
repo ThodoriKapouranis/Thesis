c7b58ab316dc8a91b18bb93d00ae502a21645c64
thodori <thodorikap+git@gmail.com>
2023-01-13 20:53:16 -0500

    updates from previous ...

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
+from dataclasses import dataclass
 import os
 import logging
 import matplotlib
 from absl import app, flags
+import numpy as np
 
 script_path = os.path.dirname(os.path.realpath(__file__))
 
@@ -10,22 +12,55 @@ font = {
     "size": 10,
 }
 
-matplotlib.style.use("classic")
+# matplotlib.style.use("classic")
 matplotlib.rc("font", **font)
 
 FLAGS = flags.FLAGS
-
-flags.DEFINE_integer("num_features", 1, "Number of features in record")
-flags.DEFINE_integer("num_samples", 50, "Number of samples in dataset")
-flags.DEFINE_integer("batch_size", 16, "Number of samples in batch")
-flags.DEFINE_integer("num_iters", 300, "Number of SGD iterations")
-flags.DEFINE_float("learning_rate", 0.1, "Learning rate / step size for SGD")
-flags.DEFINE_integer("random_seed", 31415, "Random seed")
-flags.DEFINE_float("sigma_noise", 0.5, "Standard deviation of noise random variable")
+# flags.DEFINE_integer("num_features", 1, "Number of features in record")
+# flags.DEFINE_integer("num_samples", 50, "Number of samples in dataset")
+# flags.DEFINE_integer("batch_size", 16, "Number of samples in batch")
+# flags.DEFINE_integer("num_iters", 300, "Number of SGD iterations")
+# flags.DEFINE_float("learning_rate", 0.1, "Learning rate / step size for SGD")
+# flags.DEFINE_integer("random_seed", 31415, "Random seed")
+# flags.DEFINE_float("sigma_noise", 0.5, "Standard deviation of noise random variable")
 flags.DEFINE_bool("debug", False, "Set logging level to debug")
+flags.DEFINE_integer("scenario", 1, "Training data scenario. \n\t 1: Only co_event \n\t 2: coevent & preevent \n\t 3: coevent & preevent & coherence")
+flags.DEFINE_string("model", "xgboost", "'xgboost', 'unet', 'a-unet")
+flags.DEFINE_string('S1_co', '/workspaces/Thesis/10m_data/s1_co_event_grd', 'filepath of Sentinel-1 coevent data')
+flags.DEFINE_string('S1_pre', '/workspaces/Thesis/10m_data/s1_pre_event_grd', 'filepath of Sentinel-1 prevent data')
+flags.DEFINE_string('hand_co', '/workspaces/Thesis/10m_data/coherence/hand_labeled/co_event', 'filepath of handlabelled coevent data')
+flags.DEFINE_string('hand_pre', '/workspaces/Thesis/10m_data/coherence/hand_labeled/pre_event', 'filepath of handlabelled preevent data')
+flags.DEFINE_string('S2_weak', '/workspaces/Thesis/10m_data/s2_labels', 'filepath of S2-weak labelled data')
+flags.DEFINE_string('coh_co', '/workspaces/Thesis/10m_data/coherence/co_event', 'filepath of coherence coevent data')
+flags.DEFINE_string('coh_pre', '/workspaces/Thesis/10m_data/coherence/pre_event', 'filepath of coherence prevent data')
 
+@dataclass 
+class TrainingConfig:
+    channels: int 
+    model: str
 
-def main():
+class ConfigError(Exception):
+    """Exception raised for errors in the training configuration
+    
+    Attributes:
+        flag    --  flag the error originates from.
+    """
+    def __init__(self, flag, message="Error creating configuration. Problem flags: "):
+        super().__init__(message + flag)
+
+def create_config(scenario:int, model:str):
+    if scenario not in [1,2,3]:
+        raise ConfigError("scenario")
+    channels = {1:2, 2:4, 3:6}[scenario]
+
+    if model not in ['xgboost', 'unet', 'a-unet']:
+        raise ConfigError("model")
+
+    return TrainingConfig(channels, model)
+
+def main(x):
+    config = create_config(FLAGS.scenario, FLAGS.model)
+    print(config)
     ...
     
 if __name__ == "__main__":
--- tests/dataset_statistics.py
+++ tests/dataset_statistics.py
@@ -1,29 +1,78 @@
 import os
 import rasterio
 import numpy as np
-from tqdm import tqdm
+from tqdm import tqdm, trange
+from absl import app, flags
 
 S2 = '/workspaces/Thesis/10m_data/s2_labels'
 S1_co = '/workspaces/Thesis/10m_data/s1_co_event_grd'
-S1_pre = '/workspaces/Thesis/10m_data/s1_co_event_grd'
+S1_pre = '/workspaces/Thesis/10m_data/s1_pre_event_grd'
+hand_co = '/workspaces/Thesis/10m_data/coherence/hand_labeled/co_event'
+hand_pre = '/workspaces/Thesis/10m_data/coherence/hand_labeled/pre_event'
 
 is_tif_file = lambda x: True if x[-4:]==".tif" else False
 
-'''
-Pixel Distribution of S2 Datasets
-'''
-water = 0
-non_water = 0
-invalid = 0
-for file in tqdm(os.listdir(S2)):
-    if is_tif_file(file) is False:
-        continue
+FLAGS = flags.FLAGS
+flags.DEFINE_bool("stats_label", False, "Give statistics of label dataset pixels")
+flags.DEFINE_bool("stats_data", False, "Give statistics of dataset pixels")
 
-    data = rasterio.open(S2+'/'+file, 'r').read()
-    water += len(data[data==1])
-    non_water += len(data[data==0])
-    invalid += len(data[data==-1])
+def count_labels(directory:str):
+    water = 0
+    non_water = 0
+    invalid = 0
+    for file in tqdm(os.listdir(directory)):
+        if is_tif_file(file) is False:
+            continue
 
-total = water + non_water + invalid
+        data = rasterio.open(directory+'/'+file, 'r').read()
+        water += len(data[data==1])
+        non_water += len(data[data==0])
+        invalid += len(data[data==-1])
 
-print(f'Water: {100 * water/total} \n Non_water: {100 * non_water / total} \n Invalid : {100 * invalid / total}')
+    return water, non_water, invalid
+
+def count_valid_data(directory:str):
+    '''
+    Goes through all files and grabs their max value.
+    Invalid data is numpy nan
+    '''
+    invalid = 0
+    total = 0
+    file_list = os.listdir(directory)
+    # max_hist = []
+    pbar = trange(len(file_list))
+    for i in pbar:
+        file = file_list[i]
+        if is_tif_file(file) is False:
+            continue
+        max = np.max(rasterio.open(directory+'/'+file).read())
+        # max_hist.append(max)
+        if max==0:
+            invalid += 1
+        # if np.isnan(max):
+            # invalid += 1
+
+        pbar.set_description(f'nan: {invalid}')
+        total += 1
+    
+    return invalid, total
+
+def display_label_stats(message, water, non_water, invalid):
+    total = water + non_water + invalid
+    print(f' {message} \n \
+Water: {(100*water/total):.2f} Non_water: {(100 * non_water / total):.2f} Invalid : {(100*invalid/total):.2f}')
+
+def display_data_stats(message, invalid, total):
+    print(f"""{message} \n total \t nan \t nan % \n {total} \t {invalid} \t {(100 * invalid / total):.2f}""")
+
+def main(a):
+    if FLAGS.stats_label:
+        for folder, msg in zip([S2, hand_co, hand_pre], ['s2', 'hand_co', 'hand_pre']):
+            display_label_stats(msg, *count_labels(folder))
+    
+    if FLAGS.stats_data:
+        for folder, msg in zip([S1_pre], ['s1_preevent']):
+            invalid, total = count_valid_data(folder)
+
+if __name__ == "__main__":
+    app.run(main)
