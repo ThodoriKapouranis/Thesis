b184df99d4c7bca79dc48fcc5115ce005b4809e5
thodori <thodorikap+git@gmail.com>
2023-01-23 19:01:57 -0500

    update main

--- main.py
+++ main.py
@@ -4,7 +4,9 @@ import logging
 import matplotlib
 from absl import app, flags
 import numpy as np
-from config import create_config
+from config import TrainingConfig, create_config
+from dataset_loader import create_dataset
+from models import Batched_XGBoost
 
 script_path = os.path.dirname(os.path.realpath(__file__))
 
@@ -17,16 +19,10 @@ font = {
 matplotlib.rc("font", **font)
 
 FLAGS = flags.FLAGS
-# flags.DEFINE_integer("num_features", 1, "Number of features in record")
-# flags.DEFINE_integer("num_samples", 50, "Number of samples in dataset")
-# flags.DEFINE_integer("batch_size", 16, "Number of samples in batch")
-# flags.DEFINE_integer("num_iters", 300, "Number of SGD iterations")
-# flags.DEFINE_float("learning_rate", 0.1, "Learning rate / step size for SGD")
-# flags.DEFINE_integer("random_seed", 31415, "Random seed")
-# flags.DEFINE_float("sigma_noise", 0.5, "Standard deviation of noise random variable")
+
 flags.DEFINE_bool("debug", False, "Set logging level to debug")
+
 flags.DEFINE_integer("scenario", 1, "Training data scenario. \n\t 1: Only co_event \n\t 2: coevent & preevent \n\t 3: coevent & preevent & coherence")
-flags.DEFINE_string("model", "xgboost", "'xgboost', 'unet', 'a-unet")
 flags.DEFINE_string('s1_co', '/workspaces/Thesis/10m_data/s1_co_event_grd', 'filepath of Sentinel-1 coevent data')
 flags.DEFINE_string('s1_pre', '/workspaces/Thesis/10m_data/s1_pre_event_grd', 'filepath of Sentinel-1 prevent data')
 flags.DEFINE_string('hand_co', '/workspaces/Thesis/10m_data/coherence/hand_labeled/co_event', 'filepath of handlabelled coevent data')
@@ -35,10 +31,21 @@ flags.DEFINE_string('s2_weak', '/workspaces/Thesis/10m_data/s2_labels', 'filepat
 flags.DEFINE_string('coh_co', '/workspaces/Thesis/10m_data/coherence/co_event', 'filepath of coherence coevent data')
 flags.DEFINE_string('coh_pre', '/workspaces/Thesis/10m_data/coherence/pre_event', 'filepath of coherence prevent data')
 
+# Model specific flags
+flags.DEFINE_string("model", "xgboost", "'xgboost', 'unet', 'a-unet")
+flags.DEFINE_integer('xgb_batches', 4, 'batches to use for splitting xgboost training to fit in memory')
+
 def main(x):
     config = create_config(FLAGS.scenario, FLAGS.model)
     print(config)
-    ...
+    
+    dataset = create_dataset(FLAGS)
+    batches = dataset.generate_batches(FLAGS.xgb_batches)
+    
+    if config.model == 'xgboost':
+        model = Batched_XGBoost()
+        model.train_in_batches(batches)
+
     
 if __name__ == "__main__":
     app.run(main)
