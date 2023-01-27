3eaa475e9bc7575123909c7f993a713b35ac2907
thodori <thodorikap+git@gmail.com>
2023-01-26 23:00:42 -0500

    Adds decoder block

--- Models/UNet.py
+++ Models/UNet.py
@@ -1,6 +1,6 @@
 from dataclasses import dataclass, field
 import tensorflow as tf
-from keras.layers import Conv2D, MaxPooling2D, Input
+from keras.layers import Conv2D, MaxPooling2D, Conv2DTranspose, Input
 # Could use https://github.com/yingkaisha/keras-unet-collection
 
 from keras_unet_collection import layer_utils
@@ -50,9 +50,9 @@ class UNet:
         -- Y : Dataset created by DatasetHelpers.create_dataset(). List of filenames.
         '''
         
-
 class UNetEncoderBlock(tf.keras.layers.Layer):
     # UNet Encoder block to be used with the keras layers functional api
+    #! Need to add skip connection output
     def __init__(self, filters:int, kernel_size:tuple, pool_size:tuple, name="UNet-Encoder", **kwargs):
         super().__init__(name=name, **kwargs)
         self.layer1 = Conv2D(filters=filters, kernel_size=kernel_size, activation='ReLU')
@@ -66,18 +66,34 @@ class UNetEncoderBlock(tf.keras.layers.Layer):
             inputs = self.layer3(inputs)
         return inputs
 
+class UNetDecoderBlock(tf.keras.layers.Layer):
+    # UNet Decoder block to be used with keras layers functional api
+    #! Need to add skip connection input
+    def __init__(self, filters:int, kernel_size:tuple, up_conv_size:tuple, name="UNet-Decoder", **kwargs):
+        super().__init__(name=name, **kwargs)
+        self.layer1 = Conv2DTranspose(filters, kernel_size=up_conv_size, activation='ReLU')
+        self.layer2 = Conv2D(filters=filters/2, kernel_size=kernel_size, activation='ReLU')
+        self.layer3 = Conv2D(filters=filters/2, kernel_size=kernel_size, activation='ReLU')
 
+    def call(self, inputs):
+        inputs = self.layer1(inputs)
+        inputs = self.layer2(inputs)
+        return self.layer3(inputs)
 
 def main():
     input = Input( (572,572,3) )
-
+    
     x = UNetEncoderBlock(name='EncBlk-1', filters=64, kernel_size=(3,3), pool_size=(2,2))(input)
-    x = UNetEncoderBlock(name='EncBlk-2', filters=64, kernel_size=(3,3), pool_size=(2,2))(x)
-    x = UNetEncoderBlock(name='EncBlk-3', filters=64, kernel_size=(3,3), pool_size=(2,2))(x)
-    x = UNetEncoderBlock(name='EncBlk-4', filters=64, kernel_size=(3,3), pool_size=(2,2))(x)
-    x = UNetEncoderBlock(name='EncBlk-5', filters=64, kernel_size=(3,3), pool_size=None)(x)
+    x = UNetEncoderBlock(name='EncBlk-2', filters=128, kernel_size=(3,3), pool_size=(2,2))(x)
+    x = UNetEncoderBlock(name='EncBlk-3', filters=256, kernel_size=(3,3), pool_size=(2,2))(x)
+    x = UNetEncoderBlock(name='EncBlk-4', filters=512, kernel_size=(3,3), pool_size=(2,2))(x)
 
+    x = UNetEncoderBlock(name='EncBlk-5', filters=1024, kernel_size=(3,3), pool_size=None)(x) # No further pooling, bottom of "U"
 
+    x = UNetDecoderBlock(name='DecBlk-1', filters=1024, kernel_size=(3,3), up_conv_size=(2,2))(x)
+    x = UNetDecoderBlock(name='DecBlk-2', filters=512, kernel_size=(3,3), up_conv_size=(2,2))(x)
+    x = UNetDecoderBlock(name='DecBlk-3', filters=256, kernel_size=(3,3), up_conv_size=(2,2))(x)
+    x = UNetDecoderBlock(name='DecBlk-4', filters=128, kernel_size=(3,3), up_conv_size=(2,2))(x)
 
     model = tf.keras.Model(inputs=input, outputs=x, name="UNet (just encoder actually)")
     print(model.summary())
