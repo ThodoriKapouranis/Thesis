43ac2e4034bd29686b75e833bb1b59eb9a05ccc0
thodori <thodorikap+git@gmail.com>
2023-01-27 19:30:42 -0500

    Implements skip connections

--- Models/UNet.py
+++ Models/UNet.py
@@ -55,7 +55,7 @@ class UNetEncoderBlock(tf.keras.layers.Layer):
     # Returns tuple (output, skip)
     #   -- output - Final output of the layer. Either includes a max pooling or not.
     #   -- skip   - Final output of the Conv2d layers, used as skip connection in the decoder.
-    
+
     def __init__(self, filters:int, kernel_size:tuple, pool_size:tuple, name="UNet-Encoder", **kwargs):
         super().__init__(name=name, **kwargs)
         self.layer1 = Conv2D(filters=filters, kernel_size=kernel_size, padding='same', activation='ReLU')
@@ -80,36 +80,41 @@ class UNetDecoderBlock(tf.keras.layers.Layer):
         self.skip = skip
         
         self.up = Conv2DTranspose(filters, kernel_size=kernel_size, strides=pool_size, padding='same', activation='ReLU')
-        self.conv1 = Conv2D(filters=filters/2, kernel_size=kernel_size, padding='same', activation='ReLU')
-        self.conv2 = Conv2D(filters=filters/2, kernel_size=kernel_size, padding='same', activation='ReLU')
+        self.conv1 = Conv2D(filters=filters, kernel_size=kernel_size, padding='same', activation='ReLU')
+        self.conv2 = Conv2D(filters=filters, kernel_size=kernel_size, padding='same', activation='ReLU')
 
     def call(self, inputs):
         X = self.up(inputs)
-        X = concatenate([X, self.skip])
-        X = self.conv1(X)
+        merge = concatenate([X, self.skip], axis=3)
+        print(X.shape, self.skip.shape, merge.shape)
+        print(type(X), type(self.skip), type(merge))
+
+        X = self.conv1(merge)
         X = self.conv2(X)
         return X
 
 def main():
+    tf.compat.v1.disable_eager_execution()
+
     input = Input( (512,512,3) )
 
-    enc1 = UNetEncoderBlock(name='EncBlk-1', filters=64, kernel_size=(3,3), pool_size=(2,2))(input)
-    enc2 = UNetEncoderBlock(name='EncBlk-2', filters=128, kernel_size=(3,3), pool_size=(2,2))(enc1[0])
-    enc3 = UNetEncoderBlock(name='EncBlk-3', filters=256, kernel_size=(3,3), pool_size=(2,2))(enc2[0])
-    enc4 = UNetEncoderBlock(name='EncBlk-4', filters=512, kernel_size=(3,3), pool_size=(2,2))(enc3[0])
+    enc1, skip1 = UNetEncoderBlock(name='EncBlk-1', filters=64, kernel_size=(3,3), pool_size=(2,2))(input)
+    enc2, skip2 = UNetEncoderBlock(name='EncBlk-2', filters=128, kernel_size=(3,3), pool_size=(2,2))(enc1)
+    enc3, skip3 = UNetEncoderBlock(name='EncBlk-3', filters=256, kernel_size=(3,3), pool_size=(2,2))(enc2)
+    enc4, skip4 = UNetEncoderBlock(name='EncBlk-4', filters=512, kernel_size=(3,3), pool_size=(2,2))(enc3)
 
-    enc5 = UNetEncoderBlock(name='EncBlk-5', filters=1024, kernel_size=(3,3), pool_size=None)(enc4[0]) # No further pooling, bottom of "U"
+    enc5, _ = UNetEncoderBlock(name='EncBlk-5', filters=1024, kernel_size=(3,3), pool_size=None)(enc4) # No further pooling, bottom of "U"
 
-    dec1 = UNetDecoderBlock(name='DecBlk-1', skip=enc4[1], filters=1024, kernel_size=(3,3), pool_size=(2,2))(enc5[0])
-    dec2 = UNetDecoderBlock(name='DecBlk-2', skip=enc3[1], filters=512, kernel_size=(3,3), pool_size=(2,2))(dec1)
-    dec3 = UNetDecoderBlock(name='DecBlk-3', skip=enc2[1], filters=256, kernel_size=(3,3), pool_size=(2,2))(dec2)
-    dec4 = UNetDecoderBlock(name='DecBlk-4', skip=enc1[1], filters=128, kernel_size=(3,3), pool_size=(2,2))(dec3)
+    dec1 = UNetDecoderBlock(name='DecBlk-1', skip=skip4, filters=512, kernel_size=(3,3), pool_size=(2,2))(enc5)
+    dec2 = UNetDecoderBlock(name='DecBlk-2', skip=skip3, filters=256, kernel_size=(3,3), pool_size=(2,2))(dec1)
+    dec3 = UNetDecoderBlock(name='DecBlk-3', skip=skip2, filters=128, kernel_size=(3,3), pool_size=(2,2))(dec2)
+    dec4 = UNetDecoderBlock(name='DecBlk-4', skip=skip1, filters=64, kernel_size=(3,3), pool_size=(2,2))(dec3)
 
     # Segmentation layer
     classes = 2
-    x = Conv2D(name='Dense', filters=classes, kernel_size=(1,1))(x)
+    out = Conv2D(name='Dense', filters=classes, kernel_size=(1,1))(dec4)
 
-    model = tf.keras.Model(inputs=input, outputs=x, name="UNet (just encoder actually)")
+    model = tf.keras.Model(inputs=input, outputs=out, name="UNet (just encoder actually)")
     print(model.summary())
 
 main()
