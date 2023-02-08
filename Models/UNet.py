edef430147396275e3625a16d19cf682a75c2270
thodori <thodorikap+git@gmail.com>
2023-02-08 15:18:55 -0500

    Seems to train.

--- Models/UNet.py
+++ Models/UNet.py
@@ -1,7 +1,8 @@
 from dataclasses import dataclass, field
 import tensorflow as tf
-from keras.layers import Conv2D, MaxPooling2D, Conv2DTranspose, Input, Dense, concatenate
+from keras.layers import Conv2D, MaxPooling2D, Conv2DTranspose, Input, Dense, concatenate, Dropout, BatchNormalization, Activation, Flatten
 from absl import flags, app
+from keras.regularizers import L2
 # Could use https://github.com/yingkaisha/keras-unet-collection
 # from keras_unet_collection import layer_utils
 
@@ -49,19 +50,41 @@ class UNetEncoderBlock(tf.keras.layers.Layer):
     #   -- output - Final output of the layer. Either includes a max pooling or not.
     #   -- skip   - Final output of the Conv2d layers, used as skip connection in the decoder.
 
-    def __init__(self, filters:int, kernel_size:tuple, pool_size:tuple, name="UNet-Encoder", **kwargs):
+    def __init__(self, filters:int, kernel_size:tuple, pool_size:tuple, dropout:float, name="UNet-Encoder", **kwargs):
         super().__init__(name=name, **kwargs)
-        self.layer1 = Conv2D(filters=filters, kernel_size=kernel_size, padding='same', activation='ReLU')
-        self.layer2 = Conv2D(filters=filters, kernel_size=kernel_size, padding='same', activation='ReLU')
-        self.layer3 = MaxPooling2D(pool_size=pool_size) if pool_size else None
+        self.conv1 = Conv2D(
+            filters=filters, 
+            kernel_size=kernel_size, 
+            padding='same', 
+            activation='ReLU', 
+            kernel_regularizer=L2(),
+            kernel_initializer="HeNormal"
+        )
+        self.conv2 = Conv2D(
+            filters=filters, 
+            kernel_size=kernel_size, 
+            padding='same', 
+            activation='ReLU', 
+            kernel_regularizer=L2(),
+            kernel_initializer="HeNormal"
+        )
+        self.dropout = Dropout(dropout) if dropout else None
+        self.pool = MaxPooling2D(pool_size=pool_size) if pool_size else None
 
     def call(self, inputs):
-        skip = self.layer1(inputs)
-        skip = self.layer2(skip) 
-        out = skip 
+        conv = self.conv1(inputs)
+        conv = self.conv2(conv) 
+        conv = BatchNormalization()(conv, training=False)
         
-        if self.layer3: # apply max pooling
-            out = self.layer3(out)
+        # Apply additional layers to the 'skip' connection
+        skip = conv 
+        if self.dropout:
+            skip = self.dropout(skip)
+
+        # Apply additonal layers to the convolutions to make it the final output
+        out = skip
+        if self.pool: # apply max pooling
+            out = self.pool(out)
         
         return out, skip
 
@@ -73,12 +96,30 @@ class UNetDecoderBlock(tf.keras.layers.Layer):
         self.skip = skip
         
         self.up = Conv2DTranspose(filters, kernel_size=kernel_size, strides=pool_size, padding='same', activation='ReLU')
-        self.conv1 = Conv2D(filters=filters, kernel_size=kernel_size, padding='same', activation='ReLU')
-        self.conv2 = Conv2D(filters=filters, kernel_size=kernel_size, padding='same', activation='ReLU')
-
+        self.conv1 = Conv2D(
+            filters=filters, 
+            kernel_size=kernel_size, 
+            padding='same', 
+            activation='ReLU', 
+            kernel_regularizer=L2(),
+            kernel_initializer="HeNormal"
+        )
+        self.conv2 = Conv2D(
+            filters=filters, 
+            kernel_size=kernel_size, 
+            padding='same', 
+            activation='ReLU', 
+            kernel_regularizer=L2(),
+            kernel_initializer="HeNormal"
+        )
     def call(self, inputs):
         X = self.up(inputs)
+        
+        #! #! !#
+        # merge = X
         merge = concatenate([X, self.skip], axis=3)
+        #! #! #!
+
         X = self.conv1(merge)
         X = self.conv2(X)
         return X
@@ -106,39 +147,37 @@ def _test(x):
     tf.compat.v1.disable_eager_execution()
 
     input = Input( (512,512,2) )
+    f = 32
+    enc1, skip1 = UNetEncoderBlock(name='EncBlk-1', filters=1*f, kernel_size=(3,3), pool_size=(2,2), dropout=None)(input)
+    enc2, skip2 = UNetEncoderBlock(name='EncBlk-2', filters=2*f, kernel_size=(3,3), pool_size=(2,2), dropout=None)(enc1)
+    enc3, skip3 = UNetEncoderBlock(name='EncBlk-3', filters=4*f, kernel_size=(3,3), pool_size=(2,2), dropout=0.4)(enc2)
+    enc4, skip4 = UNetEncoderBlock(name='EncBlk-4', filters=8*f, kernel_size=(3,3), pool_size=(2,2), dropout=0.4)(enc3)
 
-    enc1, skip1 = UNetEncoderBlock(name='EncBlk-1', filters=64, kernel_size=(3,3), pool_size=(2,2))(input)
-    enc2, skip2 = UNetEncoderBlock(name='EncBlk-2', filters=128, kernel_size=(3,3), pool_size=(2,2))(enc1)
-    enc3, skip3 = UNetEncoderBlock(name='EncBlk-3', filters=256, kernel_size=(3,3), pool_size=(2,2))(enc2)
-    enc4, skip4 = UNetEncoderBlock(name='EncBlk-4', filters=512, kernel_size=(3,3), pool_size=(2,2))(enc3)
+    enc5, _ = UNetEncoderBlock(name='EncBlk-5', filters=16*f, kernel_size=(3,3), pool_size=None, dropout=0.2)(enc4) # No further pooling, bottom of "U"
 
-    enc5, _ = UNetEncoderBlock(name='EncBlk-5', filters=1024, kernel_size=(3,3), pool_size=None)(enc4) # No further pooling, bottom of "U"
-
-    dec1 = UNetDecoderBlock(name='DecBlk-1', skip=skip4, filters=512, kernel_size=(3,3), pool_size=(2,2))(enc5)
-    dec2 = UNetDecoderBlock(name='DecBlk-2', skip=skip3, filters=256, kernel_size=(3,3), pool_size=(2,2))(dec1)
-    dec3 = UNetDecoderBlock(name='DecBlk-3', skip=skip2, filters=128, kernel_size=(3,3), pool_size=(2,2))(dec2)
-    dec4 = UNetDecoderBlock(name='DecBlk-4', skip=skip1, filters=64, kernel_size=(3,3), pool_size=(2,2))(dec3)
+    dec1 = UNetDecoderBlock(name='DecBlk-1', skip=skip4, filters=8*f, kernel_size=(3,3), pool_size=(2,2))(enc5)
+    dec2 = UNetDecoderBlock(name='DecBlk-2', skip=skip3, filters=4*f, kernel_size=(3,3), pool_size=(2,2))(dec1)
+    dec3 = UNetDecoderBlock(name='DecBlk-3', skip=skip2, filters=2*f, kernel_size=(3,3), pool_size=(2,2))(dec2)
+    dec4 = UNetDecoderBlock(name='DecBlk-4', skip=skip1, filters=1*f, kernel_size=(3,3), pool_size=(2,2))(dec3)
 
     # Segmentation layer
     classes = 1
-    out = Conv2D(name='Dense', filters=classes, kernel_size=(1,1))(dec4)
 
-    model = tf.keras.Model(inputs=input, outputs=out, name="UNet (just encoder actually)")
+    out = Conv2D(name='classification', filters=classes, kernel_size=(1,1), activation='sigmoid', padding='same')(dec4)
+
+    model = tf.keras.Model(inputs=input, outputs=out, name="UNet")
     print(model.summary())
 
     dataset = create_dataset(FLAGS)
-    # Modify the dataset to only use a tiny slice of data to overfit to test functionality
-    print(dataset.x_train.shape)
-    print(dataset.y_train.shape)
-    dataset.x_train, dataset.y_train = dataset.x_train[0:1], dataset.y_train[0:1]
-    print(dataset.x_train.shape)
-    print(dataset.y_train.shape)
 
+    # Modify the dataset to only use a tiny slice of data to overfit to test functionality
+    dataset.x_train, dataset.y_train = dataset.x_train[0:1], dataset.y_train[0:1]
     dataset.x_val, dataset.y_val = dataset.x_train, dataset.y_train 
+    
     train_ds, test_ds, val_ds = convert_to_tfds(dataset)
 
-    _EPOCHS = 3
-    _LR = 0.001
+    _EPOCHS = 1000
+    _LR = 1e-4
     opt = tf.keras.optimizers.Adam(
         learning_rate=_LR,
         beta_1=0.9,
@@ -147,18 +186,17 @@ def _test(x):
         amsgrad=False,
         name='Adam',
     )
-    
     model.compile(
-            loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=False),
+            loss=tf.keras.losses.BinaryCrossentropy(),
             optimizer=opt,
             metrics=['accuracy']
     )
-
+    
+    train_ds.shuffle(300)
     results = model.fit(train_ds, epochs=_EPOCHS)
     # results = model.fit(train_ds, epochs=_EPOCHS, validation_data=val_ds, validation_steps=32)
 
-    # model.save_weights("Results/Models/unet_test")
-
+    model.save_weights("Results/Models/unet_scenario1_32")
     pred = model.predict(train_ds)
     print(pred)
 
