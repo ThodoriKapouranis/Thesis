9fb2e09d9f9697325a945d21575ed62906b573ee
thodori <thodorikap+git@gmail.com>
2023-03-02 20:52:18 -0500

    create_config -> validate_config. No need for config class when flags already exist. +updates config error msg

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 from dataclasses import dataclass
 from Models.XGB import Batched_XGBoost
-
+from absl import app, flags
 
 class ConfigError(Exception):
     """Exception raised for errors in the training configuration
@@ -8,25 +8,28 @@ class ConfigError(Exception):
     Attributes:
         flag    --  flag the error originates from.
     """
-    def __init__(self, flag, message="Error creating configuration. Problem flags: "):
-        super().__init__(message + flag)
+    def __init__(self, flag, message="Error creating configuration."):
+        super().__init__(f'\033[91m {flag} \033[97m: {message}')
 
-@dataclass 
-class TrainingConfig:
-    channels: int 
-    model: str
+# @dataclass 
+# class TrainingConfig:
+#     channels: int 
+#     model: str
 
-    def initialize_model(self) -> any:
-        if self.model=='xgboost':
-            return Batched_XGBoost()
+#     def initialize_model(self) -> any:
+#         if self.model=='xgboost':
+#             return Batched_XGBoost()
 
 
-def create_config(scenario:int, model:str):
-    if scenario not in [1,2,3]:
+def validate_config(FLAGS:flags.FLAGS):
+    if FLAGS.scenario not in [1,2,3]:
         raise ConfigError("scenario")
-    channels = {1:2, 2:4, 3:6}[scenario]
+    channels = {1:2, 2:4, 3:6}[FLAGS.scenario]
 
-    if model not in ['xgboost', 'unet', 'a-unet']:
-        raise ConfigError("model")
+    if FLAGS.model not in ['xgboost', 'unet', 'a-unet']:
+        raise ConfigError("model", "Model either not supported or not defined")
+    
+    if FLAGS.savename == None:
+        raise ConfigError("savename", "Save name cannot be None ")
 
-    return TrainingConfig(channels, model)
+    return 0
