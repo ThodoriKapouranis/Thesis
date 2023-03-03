2477c90f2209e9179a5176be0c0b84631e7cfa31
thodori <thodorikap+git@gmail.com>
2023-03-02 20:51:21 -0500

    Adds UNet training option to main.py

--- main.py
+++ main.py
@@ -4,10 +4,13 @@ import logging
 import matplotlib
 from absl import app, flags
 import numpy as np
+import tensorflow as tf
+from Models.UNet import UNetCompiled
 
-from config import TrainingConfig, create_config
-from DatasetHelpers.Dataset import create_dataset
+from config import validate_config
+from DatasetHelpers.Dataset import convert_to_tfds, create_dataset
 from Models.XGB import Batched_XGBoost
+from keras.metrics import MeanIoU
 
 script_path = os.path.dirname(os.path.realpath(__file__))
 
@@ -32,20 +35,61 @@ flags.DEFINE_string('coh_co', '/workspaces/Thesis/10m_data/coherence/co_event', 
 flags.DEFINE_string('coh_pre', '/workspaces/Thesis/10m_data/coherence/pre_event', 'filepath of coherence prevent data')
 
 # Model specific flags
-flags.DEFINE_string("model", "xgboost", "'xgboost', 'unet', 'a-unet")
+flags.DEFINE_string("model", None, "'xgboost', 'unet', 'a-unet")
 flags.DEFINE_integer('xgb_batches', 4, 'batches to use for splitting xgboost training to fit in memory')
 
-def main(x):
-    config = create_config(FLAGS.scenario, FLAGS.model)
-    print(config)
-    
-    dataset = create_dataset(FLAGS)
-    batches = dataset.generate_batches(FLAGS.xgb_batches)
-    
-    if config.model == 'xgboost':
-        model = Batched_XGBoost()
-        model.train_in_batches(batches)
+# NN training Hyperparameters
+flags.DEFINE_integer("epochs", 10, "Number of epochs to train model for")
+flags.DEFINE_float("lr", 1e-4, "Defines starting learning rate")
 
+# Define model metadata
+flags.DEFINE_string("savename", None, "Name to use to save the model")
+
+def main(x):
+    validate_config(FLAGS)
     
+    # XGboost uses a different kind of dataloader than the Tensorflow models.
+    if FLAGS.model == 'xgboost':
+        model = Batched_XGBoost()
+        dataset = create_dataset(FLAGS)
+        batches = dataset.generate_batches(FLAGS.xgb_batches)
+        model.train_in_batches(batches)
+    
+    else:
+        # Generic tensorflow NN hyperparameter and dataset creation
+        model=None
+        dataset = create_dataset(FLAGS)
+        train_ds, val_ds, test_ds, = convert_to_tfds(dataset)
+        
+        lr_schedule = tf.keras.optimizers.schedules.ExponentialDecay(
+            FLAGS.lr,
+            decay_steps=200,
+            decay_rate=0.96,
+            staircase=True
+        )
+        
+        opt = tf.keras.optimizers.Adam(
+            learning_rate=lr_schedule,
+            beta_1=0.9,
+            beta_2=0.999,
+            epsilon=1e-07,
+            amsgrad=False,
+            name='Adam',
+        )
+
+        if FLAGS.model == 'unet':
+            model = UNetCompiled(input_size=(512,512,2), n_filters=64, n_classes=2)
+            print(model.summary())
+            
+            model.compile(
+                loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+                optimizer=opt,
+                metrics=[MeanIoU(num_classes=2, sparse_y_pred=False)]
+            )
+
+
+        results = model.fit(train_ds, epochs=FLAGS.epochs, validation_data=val_ds, validation_steps=32)
+        model.save(f"Results/Models/{FLAGS.savename}")
+
 if __name__ == "__main__":
     app.run(main)
